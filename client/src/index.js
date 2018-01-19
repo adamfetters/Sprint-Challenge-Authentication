221a831117a9d0bb941c1af5a4a615a1610e9270
Adam Fetters <adamfetterscoder@gmail.com>
2018-01-19 17:12:00 -0500

    Index.js setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
+import Jokes from './components/Jokes/Joke';
+import { Provider } from 'react-redux';
+import { createStore , applyMiddlewre } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { 
+  BrowserRoute as Router,
+  Route
+  } from 'react-router-dom';
+  import reducers from './reducers';
+import './index.css';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router>
+      <div>
+        <Route exact path='/' component={App} />
+        <Route path='/signup' component={SignUp} />
+        <Route path='/signin' component={SignIn} />
+        <Route path='/jokes' component={Jokes} />
+      </div>
+    </Router>
+  </Provider>,
+ document.getElementById('root'));
 
